05dec427cb92074099a89ea0428587d0505db490
LAZ-R <crazydrawer62@gmail.com>
2021-05-14 09:47:53 +0200

    Commit initial
    Création de la classe Attaque

---
+++ src/fr/eni/quelPokemon/bo/Attaque.java
@@ -0,0 +1,48 @@
+package fr.eni.quelPokemon.bo;
+
+/** Classe qui représente une Attaque.
+ *
+ * @author laz_R
+ * @version 1.0
+ */
+public class Attaque {
+
+    // Attributs d'instances
+    private String nom;
+    private int pointsDeDegats;
+
+    // Attributs de classe
+
+
+    // Méthodes
+
+    /** Constructeur avec tous les paramètres.
+     *
+     * @param nom String
+     * @param pointsDeDegats int
+     */
+    public Attaque(String nom, int pointsDeDegats) {
+        this.nom = nom;
+        this.pointsDeDegats = pointsDeDegats;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPointsDeDegats() {
+        return this.pointsDeDegats;
+    }
+
+    public void setPointsDeDegats(int pointsDeDegats) {
+        this.pointsDeDegats = pointsDeDegats;
+    }
+
+    public void afficher() {
+        System.out.printf("L'attaque %s inflige %d points de dégâts.%n", this.nom, this.pointsDeDegats);
+    }
+}
