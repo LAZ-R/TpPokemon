a9e59225fcb532bb4c2d0401524a8940c7de7399
LAZ-R <crazydrawer62@gmail.com>
2021-05-14 10:08:58 +0200

    Création de la classe Pokemon.

---
+++ src/fr/eni/quelPokemon/bo/Pokemon.java
@@ -0,0 +1,137 @@
+package fr.eni.quelPokemon.bo;
+
+/** Classe qui représente un Pokémon.
+ *
+ * @author laz_R
+ * @version 1.0
+ */
+public class Pokemon {
+
+    // Attributs d'instances
+    private String nom;
+    private int taille;
+    private int poids;
+    private int pv;
+
+    private Attaque attaque01;
+    private Attaque attaque02;
+
+    private Dresseur dresseur;
+
+    // Attributs de classe
+
+    // Méthodes
+
+    /** Constructeur avec tous les paramètres, sauf Dresseur.
+     *
+     * @param nom String
+     * @param taille int
+     * @param poids int
+     * @param pv int
+     * @param attaque01 Attaque
+     * @param attaque02 Attaque
+     */
+    public Pokemon(String nom, int taille, int poids, int pv, Attaque attaque01, Attaque attaque02) {
+        this.nom = nom;
+        this.taille = taille;
+        this.poids = poids;
+        this.pv = pv;
+        this.attaque01 = attaque01;
+        this.attaque02 = attaque02;
+        this.dresseur = null;
+    }
+
+    /** Constructeur avec tous les paramètres.
+     *
+     * @param nom String
+     * @param taille int
+     * @param poids int
+     * @param pv int
+     * @param attaque01 Attaque
+     * @param attaque02 Attaque
+     * @param dresseur Dresseur
+     */
+    public Pokemon(String nom, int taille, int poids, int pv, Attaque attaque01, Attaque attaque02, Dresseur dresseur) {
+        this.nom = nom;
+        this.taille = taille;
+        this.poids = poids;
+        this.pv = pv;
+        this.attaque01 = attaque01;
+        this.attaque02 = attaque02;
+        this.dresseur = dresseur;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getTaille() {
+        return this.taille;
+    }
+
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    public int getPoids() {
+        return this.poids;
+    }
+
+    public void setPoids(int poids) {
+        this.poids = poids;
+    }
+
+    public int getPv() {
+        return this.pv;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+
+    public Attaque getAttaque01() {
+        return this.attaque01;
+    }
+
+    public void setAttaque01(Attaque attaque01) {
+        this.attaque01 = attaque01;
+    }
+
+    public Attaque getAttaque02() {
+        return this.attaque02;
+    }
+
+    public void setAttaque02(Attaque attaque02) {
+        this.attaque02 = attaque02;
+    }
+
+    public Dresseur getDresseur() {
+        return this.dresseur;
+    }
+
+    public void setDresseur(Dresseur dresseur) {
+        this.dresseur = dresseur;
+    }
+
+    /** Méthode d'affichage des informations d'un Pokémon.
+     */
+    public void afficher() {
+        System.out.println(this.nom);
+        System.out.println(this.taille);
+        System.out.println(this.poids);
+        System.out.println(this.pv);
+        System.out.println("Attaque 01 :");
+        this.attaque01.afficher();
+        System.out.println("Attaque 02 :");
+        this.attaque02.afficher();
+        if (this.dresseur != null) {
+            System.out.println("Dresseur :");
+            this.dresseur.afficher();
+        }
+
+    }
+}
